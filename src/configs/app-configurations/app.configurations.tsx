b7de09614e42b66bb5c50d72f46b884237d71202
gh.hassanzadeh <gholamreza15402@gmail.com>
2023-12-02 14:52:15 +0330

    add font family in seytem

--- src/configs/app-configurations/app.configurations.tsx
+++ src/configs/app-configurations/app.configurations.tsx
@@ -1,6 +1,7 @@
 // * Import Casual modules
 import { FC } from "react";
-
+/* @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
+ */
 // * Import interfase
 import { IAppConfigurations } from "@configs/app-configurations/app.configurations.interfaces";
 export const AppConfigurations: FC<IAppConfigurations> = () => {
---
+++ src/configs/app-configurations/base-styles/base-styles.config.tsx
@@ -0,0 +1,23 @@
+// * Import Tools
+import { css } from "@emotion/react";
+import { Global } from "@emotion/react";
+
+// * Import config reset style
+import { BaseStylesConfigReset } from "@configs/app-configurations/base-styles/base-styles.config.reset";
+export const BaseStylesConfig = () => {
+    return (
+        <>
+            <Global styles={BaseStylesConfigReset} />
+            <Global
+                styles={css`
+                    @font-face {
+                        font-family: Roboto;
+                        font-style: normal;
+                        font-weight: 700;
+                        src: url(https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap);
+                    }
+                `}
+            />
+        </>
+    );
+};
