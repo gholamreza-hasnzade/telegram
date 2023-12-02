64aac9f638b0621cfe23b0b09bc92299913820e0
gh.hassanzadeh <gholamreza15402@gmail.com>
2023-12-02 15:47:19 +0330

    add package react router dom

--- src/configs/app-configurations/app.configurations.tsx
+++ src/configs/app-configurations/app.configurations.tsx
@@ -1,13 +1,13 @@
 // * Import Casual modules
 import { FC } from "react";
-/* @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;700&display=swap');
- */
+
 // * Import interfase
 import { IAppConfigurations } from "@configs/app-configurations/app.configurations.interfaces";
-
+// * import configs
 import { EmotionConfig } from "@configs/app-configurations/emotion/emotion.config";
 import { BaseStylesConfig } from "@configs/app-configurations/base-styles/base-styles.config";
 import { MaterialUIConfig } from "@configs/app-configurations/material-ui/material-ui.config";
+
 export const AppConfigurations: FC<IAppConfigurations> = ({ children }) => {
     return (
         <MaterialUIConfig>
