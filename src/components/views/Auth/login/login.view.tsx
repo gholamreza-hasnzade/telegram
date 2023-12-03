36a5e28f39f499cebbf85f9694d7660101727e2e
gh.hassanzadeh <gholamreza15402@gmail.com>
2023-12-03 13:30:32 +0330

    delete code

--- src/components/views/Auth/login/login.view.tsx
+++ src/components/views/Auth/login/login.view.tsx
@@ -1,11 +1,5 @@
 /* import React from 'react' */
 
-import { TextField } from "@components/atoms";
-
 export const LoginView = () => {
-    return (
-        <div>
-            <TextField label="test" placeholder="test" variant="outlined" type="password" />
-        </div>
-    );
+    return <div>LoginView</div>;
 };
