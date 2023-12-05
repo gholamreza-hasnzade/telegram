b28912352a1f3e51c7a197d11e2a72cb42350f26
gh.hassanzadeh <gholamreza15402@gmail.com>
2023-12-05 16:21:58 +0330

    start header part

--- src/components/organisms/header/header.organism.style.tsx
+++ src/components/organisms/header/header.organism.style.tsx
@@ -1,3 +1,17 @@
+// * Import Tools
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+// * import interface
+
+const Header = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90px;
+    background-color: ${({ theme }) => theme.TEXT_PRIMARY};
+`;
+
 export const HeaderOrganismStyle = {
-    
-}
+    Header,
+};
--- src/components/organisms/header/header.organism.tsx
+++ src/components/organisms/header/header.organism.tsx
@@ -1,7 +1,11 @@
-
-
+// * import style
+import { HeaderOrganismStyle as S } from "@components/organisms/header/header.organism.style";
 export const HeaderOrganism = () => {
-  return (
-    <div>HeaderOrganisms</div>
-  )
-}
+    return (
+        <S.Header>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+        </S.Header>
+    );
+};
