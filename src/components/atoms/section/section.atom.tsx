1e95c233b106e142f595f492daa91f27f0ab84b4
gh.hassanzadeh <gholamreza15402@gmail.com>
2023-12-09 14:02:21 +0330

    delete component

--- src/components/atoms/section/section.atom.tsx
+++ src/components/atoms/section/section.atom.tsx
@@ -6,13 +6,10 @@ import { ISection } from "@components/atoms/section/section.atom.interface";
 
 // * import style
 import { SectionAtomStyle as S } from "@components/atoms/section/section.atom.style";
-import { DrawerAtom } from "..";
 
 export const SectionAtom: FC<ISection> = ({ children, height }) => {
     return (
         <S.Section height={height}>
-            <DrawerAtom />
-
             {children}
         </S.Section>
     );
