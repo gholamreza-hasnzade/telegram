63911989953dbca95444ef63bf97c3a86b47b598
Gholamreza Hassanzadeh Tabrizi <gholamreza.hassanzade.1994@gmail.com>
2024-06-26 12:43:28 +0330

    config all mui and emctions style

--- src/configs/app-configurations/app.configurations.tsx
+++ src/configs/app-configurations/app.configurations.tsx
@@ -1,6 +1,18 @@
+// * Import tools
+import { FC } from "react";
+// * Import interfase
+import { IAppConfigurations } from "@/configs/app-configurations/app.configurations.interface";
 
-export const AppConfigurations = () => {
+// * import configs
+import { EmotionConfig } from "@/configs/app-configurations/emotion/emotion.config";
+import { BaseStylesConfig } from "@/configs/app-configurations/base-styles/base-styles.config";
+import { MaterialUIConfig } from "@/configs/app-configurations/mui/material-ui";
+
+export const AppConfigurations: FC<IAppConfigurations> = ({ children }) => {
   return (
-    <div>AppConfigurations</div>
-  )
-}
+    <MaterialUIConfig>
+      <BaseStylesConfig />
+      <EmotionConfig>{children}</EmotionConfig>
+    </MaterialUIConfig>
+  );
+};
